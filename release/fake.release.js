c2961621af806a4ef07201adb2248561c175a95f
Daio-io <dai.williams@bbc.co.uk>
2015-10-29 14:04:16 +0000

    tetsing commit

--- release/fake.release.js
+++ release/fake.release.js
@@ -1,16 +1,16 @@
 'use strict';
 
 var FakeContent = function () {
-  
-  this.isFake = 'some fake var';
-  
-  
+
+  this.isFake = 'some fake var testing';
+
+
 };
 
 FakeContent.prototype.getFake = function () {
-  
+
   return this.isFake;
-  
+
 };
 
 module.exports = FakeContent;
